da02fa3f9fbcee7056de8bc386a056aacac3175c
Haziq <haziqmk123@gmail.com>
2026-01-21 17:24:10 +0400

    Added other models

--- AceGPT.py
+++ AceGPT.py
@@ -1,8 +1,8 @@
 from EGRA_functions import EGRA
 
-class Jais(EGRA):
+class AceGPT(EGRA):
     def __init__(self):
-        super().__init__(model="inceptionai/Jais-2-70B-Chat")
+        super().__init__(model="i/FreedomIntelligence/AceGPT-v2-70B-Chat")
 
 
     def zero_shot(self, output_file="example.csv", num_stories=1, max_new_tokens=100, do_sample=True, include_sys=True):
--- Qween.py
+++ Qween.py
@@ -1,8 +1,8 @@
 from EGRA_functions import EGRA
 
-class Jais(EGRA):
+class Qween(EGRA):
     def __init__(self):
-        super().__init__(model="inceptionai/Jais-2-70B-Chat")
+        super().__init__(model="Raido/qween7.5-arabic-story-teller-2")
 
 
     def zero_shot(self, output_file="example.csv", num_stories=1, max_new_tokens=100, do_sample=True, include_sys=True):
